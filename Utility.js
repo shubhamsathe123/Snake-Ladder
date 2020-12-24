f1a24f32781b64fdc8d612cd4c03f0eb4b97ff85
shubhamsathe123 <shubhamsathe66@gmail.com>
2020-12-24 12:55:19 +0530

    play upto getting exact winning position

--- Utility.js
+++ Utility.js
@@ -13,26 +13,26 @@ class Utility
      while(this.current_position<this.END_POSITION)
      {
        let dice=Math.floor(Math.random()*6)+1;
-       console.log(`check option | ${dice}`);
+       console.log(`dice | ${dice}`);
        let check_option=Math.floor(Math.random()*3)+1;
        switch(check_option)
          {
            case this.SNAKE:
-                console.log('snake');
-               if(this.current_position-dice<=100)
+                console.log('snake |');
+               if(this.current_position-dice>=this.START_POSITION)
                {
                 this.current_position=this.current_position-dice;
                }
                break;
            case this.LADDER:
-               console.log('ladder');
-               if(this.current_position+dice<=100)
+               console.log('ladder |');
+               if(this.current_position+dice<=this.END_POSITION)
                {
                this.current_position=this.current_position+dice;
                } 
               break;
            case this.NO_PLAY:
-               console.log('no play');
+               console.log('no play |');
                this.current_position=this.current_position;
                break;
          }
