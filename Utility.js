3c2ffa4723c79b21ef5ca1213c3a040ead98b9cf
shubhamsathe123 <shubhamsathe66@gmail.com>
2020-12-24 12:08:10 +0530

    dice rolled out

--- Utility.js
+++ Utility.js
@@ -2,9 +2,11 @@ class Utility
 {
    START_POSITION = 0;
    END_POSITION = 100;
+   //uc1
     gamePlay = () =>
     {
-       
+       let dice=Math.floor(Math.random()*6)+1;
+       console.log(`number getting after rolling the dice is ${dice}`);
     }
 }
 module.exports = new Utility();
